f168c13ea53af65826ae6696e0892aae8e892915
wskysan134-debug <wskysan134@gmail.com>
2026-01-07 23:53:11 +0200

    Update logger.js

--- logger/logger.js
+++ logger/logger.js
@@ -1,7 +1,31 @@
 const fs = require('fs-extra');
 const path = require('path');
 const moment = require('moment');
-const logDir = path.join(__dirname, 'logs');fs.ensureDirSync(logDir);const logFile = path.join(logDir, `log_${moment().format('YYYYMMDD')}.log`);const log = (level, message) => {  const timestamp = moment().format('YYYY-MM-DD HH:mm:ss');  const logMessage = `[${timestamp}] [${level.toUpperCase()}] ${message}
-`;  fs.appendFileSync(logFile, logMessage, { encoding: 'utf8' });  switch (level) {    case 'info':      console.log(logMessage);      break;    case 'error':      console.error(logMessage);      break;    case 'warn':      console.warn(logMessage);      break;  }};
+
+const logDir = path.join(__dirname, 'logs');
+fs.ensureDirSync(logDir);
+
+const logFile = path.join(logDir, `log_${moment().format('YYYYMMDD')}.log`);
+
+const log = (level, message) => {
+  const timestamp = moment().format('YYYY-MM-DD HH:mm:ss');
+  const logMessage = `[${timestamp}] [${level.toUpperCase()}] ${message}\n`;
+
+  // حفظ السجل في الملف
+  fs.appendFileSync(logFile, logMessage, { encoding: 'utf8' });
+
+  // طباعة السجل في الكونسول مع تعريب النوع
+  switch (level) {
+    case 'info':
+      console.log(`[INFO] [معلومات] ${timestamp} - ${message}`);
+      break;
+    case 'error':
+      console.error(`[ERROR] [خطأ] ${timestamp} - ${message}`);
+      break;
+    case 'warn':
+      console.warn(`[WARN] [تحذير] ${timestamp} - ${message}`);
+      break;
+  }
+};
 
 module.exports = { log };
